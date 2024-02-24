baf3b7cf82e105c5b639d3eb26508e24229a56d2
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2024-02-24 12:43:09 +0200

    Hero title and subtitle

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,17 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="hero">
+        <div className="flex-1 pt-36 padding-x">
+            <h1 className="hero__title">
+                Encontre, reserve ou alugue um carro — rapidamente e facilmente!
+            </h1>
+
+            <p className="hero__subtitle">Otimize sua experiência de aluguel de carro com nosso processo de reserva sem esforço.</p>
+
+            
+        </div>
+    </div>
   )
 }
 
