1f3334bb18e3cb9a5348f4e450e9f100d80e07f4
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2024-02-26 17:20:02 +0200

    Cars catalogue

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,21 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Catálogo de Carros</h1>
+          <p>Explore os carros que podem te interessar</p>
+        </div>
+
+        <div className="home__filters">
+          <SearchBar />
+
+          <div className="home__filter-container">
+            <CustomFilter title="fuel"/>
+            <CustomFilter title="year"/>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
