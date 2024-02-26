da148bd91dbf5ba852634a05809ab188e84711db
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2024-02-26 20:20:34 +0200

    Combombox Button

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,9 +1,29 @@
+"use client"
+
+import { Combobox, Transition } from '@headlessui/react';
+
 import { searchManufacturerProps } from '@/types';
-import React from 'react'
+import Image from 'next/image';
+
 
 const SearchManufacturer = ({manufacturer, setManufacturer}: searchManufacturerProps) => {
   return (
-    <div>SearchManufacturer</div>
+    <div className='search-manufacturer'>
+        <Combobox>
+            <div className='relative w-full'>
+                <Combobox.Button className="absolute top-[14px]">
+                    <Image 
+                        src="/car-logo.svg"
+                        width={20}
+                        height={20}
+                        className='ml-4'
+                        alt='Car logo'
+                    />
+                </Combobox.Button>
+                
+            </div>
+        </Combobox>
+    </div>
   )
 }
 
