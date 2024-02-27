31612a8b740cc9d9e99a3e0e89d68d71ee47f992
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2024-02-27 12:05:12 +0200

    Creating the transition logic

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,15 +1,22 @@
 "use client"
-import { useState } from 'react';
+import { useState, Fragment } from 'react';
 
 import { Combobox, Transition } from '@headlessui/react';
+import Image from 'next/image';
 
 import { searchManufacturerProps } from '@/types';
-import Image from 'next/image';
+import { manufacturers } from '@/constants';
 
 
 const SearchManufacturer = ({manufacturer, setManufacturer}: searchManufacturerProps) => {
 
     const [query, setQuery] = useState("");
+
+    const filteredManufacturers = query === "" ? manufacturers : manufacturers.filter((item) => (
+        item.toLowerCase()
+        .replace(/\s+/g, "")
+        .includes(query.toLowerCase().replace(/\s+/g, "")
+    )));
 
 
 
@@ -33,7 +40,29 @@ const SearchManufacturer = ({manufacturer, setManufacturer}: searchManufacturerP
                     onChange={(e) => setQuery(e.target.value)}
                 />
 
-                
+                <Transition
+                    as={Fragment}
+                    leave='transition ease-in duration-100'
+                    leaveFrom='opacity-100'
+                    leaveTo='opacity-0'
+                    afterLeave={() => setQuery('')}
+                >
+                    <Combobox.Options>
+                        {
+                            filteredManufacturers.map((item) => (
+                                <Combobox.Option
+                                    key={item}
+                                    className={({ active }) => `
+                                        relative searchManufacturer__option ${active ? 'bg-primary-blue text-white' : 'text-gray-900'}
+                                    `}
+                                    value={item}
+                                >
+                                    {item}
+                                </Combobox.Option>
+                            )
+                        )}
+                    </Combobox.Options>
+                </Transition>                             
             </div>
         </Combobox>
     </div>
