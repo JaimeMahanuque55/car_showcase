edbc060f3027ae4682abcccd52e3b00e318ee83e
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2024-02-28 11:32:59 +0200

    transmission, drive and gas displaying

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -42,8 +42,27 @@ const CarCard = ({ car }: CarCardProps) => {
                 />
         </div>
 
-        <div>
-            
+        <div className='relative flex w-full mt-2'>
+            <div className='flex group-hover:invisible w-full justify-between text-gray'>
+                <div className='flex flex-col justify-center items-center gap-2'>
+                    <Image src="/steering-wheel.svg" width={20} height={20} alt='steering wheel'/>
+                    <p className='text-[14px]'>
+                        {transmission === 'a' ? 'Automatico' : 'Manual'}
+                    </p>
+                </div>
+                <div className='flex flex-col justify-center items-center gap-2'>
+                    <Image src="/tire.svg" width={20} height={20} alt='Tire'/>
+                    <p className='text-[14px]'>
+                        {drive.toUpperCase()}
+                    </p>
+                </div>
+                <div className='flex flex-col justify-center items-center gap-2'>
+                    <Image src="/gas.svg" width={20} height={20} alt='steering wheel'/>
+                    <p className='text-[14px]'>
+                        {city_mpg} MPG
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
   )
