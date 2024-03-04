0f1dff32a1d71c508d4906dcda7c24b634b77a81
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2024-03-04 14:04:57 +0200

    editing the url params

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,14 @@
 import { CarProps, FilterProps } from "@/types";
 
 export async function fetchCars(filters: FilterProps) {
+    const { manufacturer, year, model, limit, fuel } = filters;
+
     const headers = {
         'X-RapidAPI-Key': '931d680df0msh1eab21a79ac5631p16c900jsn2b756634e5ad',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
 
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3', {
+    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&${limit}&fuel_type=${fuel}`, {
         headers: headers,
     });
 
